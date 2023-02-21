fb06e97e6af1a5611c59c9b40efb325881719eeb
Swabir <swabir49@gmail.com>
2023-02-21 10:24:06 +0300

    forms in git

--- Swabir_MIT/views.py
+++ Swabir_MIT/views.py
@@ -8,10 +8,10 @@ def reg(request):
     email = ''
     password = ''
 
-
     reg_form = UserReg(request.POST or None)
     if reg_form.is_valid():
         name = reg_form.cleaned_data.get("name")
         email = reg_form.cleaned_data.get("email")
         password = reg_form.cleaned_data.get("password")
-
+    context = {'reg_form': reg_form, 'name': name, 'email': email, 'submit_button': submit_button}
+    return render(request, 'register.html', context)
