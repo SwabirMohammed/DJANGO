0813bd81c0967c85dd1e20779c13305cc416a3d4
Swabir <swabir49@gmail.com>
2023-02-22 09:51:48 +0300

    forms in git

--- Swabir_MIT/forms.py
+++ Swabir_MIT/forms.py
@@ -14,4 +14,7 @@ class UserLogin(forms.Form):
 
 class MembersReg(forms.Form):
     name = forms.CharField(max_length=100)
-    email = forms.CharField(max_length=100)
+    age = forms.CharField(max_length=100)
+    phone = forms.CharField(max_length=100)
+    city = forms.CharField(max_length=100)
+    country = forms.CharField(max_length=100)
